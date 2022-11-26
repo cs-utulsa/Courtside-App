85c946992899f4398187b69338283ff272ae97aa
ben-rieth <benrieth3@gmail.com>
2022-11-25 18:34:27 -0600

    Make jwts expire after 1 week

--- backend/utils/jwt_utils.py
+++ backend/utils/jwt_utils.py
@@ -6,7 +6,7 @@ from db import db
 def encode_auth_token(user_id):
     try:
         payload = {
-            'exp': datetime.utcnow() + timedelta(weeks=2),
+            'exp': datetime.utcnow() + timedelta(days=1),
             'iat': datetime.utcnow(),
             'sub': user_id
         }
