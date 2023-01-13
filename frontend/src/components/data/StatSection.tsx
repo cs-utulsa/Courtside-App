fbc8efd5d9d61729d44118a4e441fc784e75276b
ben-rieth <benrieth3@gmail.com>
2023-01-13 15:24:17 -0600

    Add jsdoc documentation to StatSection

--- frontend/src/components/data/StatSection.tsx
+++ frontend/src/components/data/StatSection.tsx
@@ -4,13 +4,39 @@ import { View, Pressable, Text, StyleSheet } from 'react-native';
 import AntIcon from '@expo/vector-icons/AntDesign';
 
 type StatSectionProps = {
+    /** the title of the section */
     title: string;
+    /** the stats that are within this section */
     data: { id: string; name: string }[];
+    /** the stats that are currently selected, this is stored as state in the parent component */
     selectedStats: string[];
+    /** method that adds a stat to the list of currently selected stats, defined in parent component  */
     addStat: (stat: string) => void;
+    /** method that removes a stat from the list of currently selected stats, defined in parent component  */
     removeStat: (stat: string) => void;
 };
 
+/**
+ * Component that shows a list of stats as a list of buttons that can be toggled.
+ * This component is meant to be used as the renderItem in a SectionList
+ *
+ * @component
+ * @example
+ * const title = "Defense Stats"
+ * const data = [{id: "1", name: "Stat 1"}, {"id": 2, name: "Stat 2"}]
+ * const selectedStats = ["1", "3", "0"]
+ * const addStat = (stat: string) => { selectedStats.push(stat) }
+ * const removeStat = (stat: string) => { selectedStats.filter((s) => s !== stat)}
+ * return (
+ *      <StatSection
+ *          title={title}
+ *          data={data}
+ *          selectedStats={selectedStats}
+ *          addStat={addStat}
+ *          removeStat={removeStat}
+ *      />
+ * );
+ */
 export const StatSection: FC<StatSectionProps> = ({
     title,
     data,
