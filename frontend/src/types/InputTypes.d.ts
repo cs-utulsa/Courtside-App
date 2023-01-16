8376ad46f469f947cdc54e24280b5a68f358e173
ben-rieth <benrieth3@gmail.com>
2023-01-15 19:27:14 -0600

    Add JSdoc for all auth components

--- frontend/src/types/InputTypes.d.ts
+++ frontend/src/types/InputTypes.d.ts
@@ -1,10 +1,16 @@
 import { NativeSyntheticEvent, TextInputFocusEventData } from 'react-native';
 
 export type InputProps = {
+    /** This function is called when the input is changed */
     changeFn: (field: string) => void;
+    /** THis function is called when the user leaves the field (the opposite of focusing) */
     blurFn: (e: NativeSyntheticEvent<TextInputFocusEventData>) => void;
+    /** The error text */
     error: string | undefined;
+    /** Whether or not the input has been focused by the user yet */
     touched: boolean | undefined;
+    /** The current value of the input */
     value: string;
+    /** Whether or not the input can be accessed by the user */
     disabled: boolean;
 };
