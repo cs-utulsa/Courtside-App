30a1a24681f86b5dd6e2d1eb04e76c0340a8d3de
ben-rieth <benrieth3@gmail.com>
2023-01-15 19:29:50 -0600

    JSDoc the styles for inputs

--- frontend/src/styles/inputStyles.ts
+++ frontend/src/styles/inputStyles.ts
@@ -1,10 +1,12 @@
 import { StyleSheet } from 'react-native';
 
 export const inputStyles = StyleSheet.create({
+    /** Styles for the view that contains the input*/
     container: {
         width: '100%',
         alignItems: 'center',
     },
+    /** Styles for the input box itself */
     input: {
         fontSize: 20,
         paddingHorizontal: 10,
@@ -15,9 +17,11 @@ export const inputStyles = StyleSheet.create({
         marginTop: 15,
         marginBottom: 2,
     },
+    /** Styles that are added to the input when the input's value has an error */
     inputError: {
         borderColor: 'red',
     },
+    /** Styles for the error text that appears beneath the button when there is an error */
     errorText: {
         color: 'red',
     },
