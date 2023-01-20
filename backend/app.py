309fa36e4b563a1b599f23fbc59070ddc7d5197e
ben-rieth <benrieth3@gmail.com>
2023-01-20 14:25:12 -0600

    Restore old leaderboard route

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,38 @@ def get_roster(team_code):
 def get_leaderboard(stat, per_mode):
     return json.dumps(db.leaderboards.find_one({'_id': f'{stat}_{per_mode}'}))
 
+# Return leaderboard for specified stat/season
+@app.route('/leaderboard/<stat>', methods=['GET'])
+def get_leaderboard(stat):
+    leaderboard = db.leaderboards.find_one({'_id': f'{stat}'})
+    leaderboard["player_id"] = leaderboard["player_id"][0:5]
+    leaderboard["value"] = leaderboard["value"][0:5]
+
+    player_names_cursor = db.player_data.find(
+        { "_id": { "$in": leaderboard["player_id"]}}
+    )
+
+    player_document = list(player_names_cursor)
+
+    if len(player_document) == 0:
+        leaderboard["player_names"] = leaderboard["player_id"]
+        return leaderboard
+
+    leaderboard["player_names"] = []
+    cursor = 0
+    # loop over player ids and only return a player name if it is present, this code is necessary since all of the players are not in the database
+    for i in range(0, len(leaderboard["player_id"])):
+        if cursor > len(player_document) - 1:
+            leaderboard["player_names"].append(leaderboard["player_id"][i])
+        elif (leaderboard["player_id"][i] == player_document[cursor]["data"][0]):
+            name = player_document[cursor]["data"][1]
+            leaderboard["player_names"].append(name)
+            cursor += 1
+        else:
+            leaderboard["player_names"].append(leaderboard["player_id"][i])
+
+    return leaderboard
+
 # Return schedule for a requested day
 @app.route('/schedule/<int:month>/<int:day>', methods=['GET'])
 def get_schedule(month, day):
