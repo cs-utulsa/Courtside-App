cd09166be22b32422d5401b23d2416ee2fe8a3c5
RyleeBuchert <ryleebuchert@gmail.com>
2023-01-19 21:00:33 -0600

    Updated player bio data and Flask API

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
 from auth import auth as auth_blueprint
-from flask import Flask, jsonify
+from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
 from db import db
 import pandas as pd
-import pymongo
-import os
+import json
 from utils.data_utils import schedule_key
 
 app = Flask(__name__)
@@ -18,37 +17,11 @@ app.register_blueprint(auth_blueprint)
 def get_roster(team_code):
     return db.teams.find_one({'_id': team_code})['roster']
 
-# Return leaderboard for specified stat/season
-@app.route('/leaderboard/<stat>', methods=['GET'])
-def get_leaderboard(stat):
-    leaderboard = db.leaderboards.find_one({'_id': f'{stat}'})
-    leaderboard["player_id"] = leaderboard["player_id"][0:5]
-    leaderboard["value"] = leaderboard["value"][0:5]
-
-    player_names_cursor = db.player_data.find(
-        { "_id": { "$in": leaderboard["player_id"]}}
-    )
-
-    player_document = list(player_names_cursor)
-    
-    if len(player_document) == 0:
-        leaderboard["player_names"] = leaderboard["player_id"]
-        return leaderboard
-
-    leaderboard["player_names"] = []
-    cursor = 0
-    # loop over player ids and only return a player name if it is present, this code is necessary since all of the players are not in the database
-    for i in range(0, len(leaderboard["player_id"])):
-        if cursor > len(player_document) - 1:
-            leaderboard["player_names"].append(leaderboard["player_id"][i])
-        elif (leaderboard["player_id"][i] == player_document[cursor]["data"][0]):
-            name = player_document[cursor]["data"][1]
-            leaderboard["player_names"].append(name)
-            cursor += 1
-        else:
-            leaderboard["player_names"].append(leaderboard["player_id"][i])
-
-    return leaderboard
+# Return leaderboard for specified stat
+# --- per_mode can be either tot, pg, or p48
+@app.route('/leaderboard/<stat>/<per_mode>', methods=['GET'])
+def get_leaderboard(stat, per_mode):
+    return json.dumps(db.leaderboards2.find_one({'_id': f'{stat}_{per_mode}'}))
 
 # Return schedule for a requested day
 @app.route('/schedule/<int:month>/<int:day>', methods=['GET'])
@@ -63,9 +36,9 @@ def get_schedule(month, day):
     return res
 
 # Return bio data for a specified player
-@app.route('/player/<player_id>', methods=['GET'])
+@app.route('/player/<int:player_id>', methods=['GET'])
 def get_player_data(player_id):
-    return db.player_data.find_one({'_id': player_id})
+    return json.dumps(db.players.find_one({'_id': player_id}))
 
 # Main method
 if __name__ == "__main__":
