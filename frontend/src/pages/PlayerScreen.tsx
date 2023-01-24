3a431d83681d7295cfa2a3b74db1498d61f5e3bc
ben-rieth <benrieth3@gmail.com>
2023-01-24 16:11:10 -0600

    Add headshot to PlayerScreen

--- frontend/src/pages/PlayerScreen.tsx
+++ frontend/src/pages/PlayerScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useRoute } from '@react-navigation/native';
 import { View, Text, StyleSheet } from 'react-native';
 import { Player } from './../types/Player';
+import { CircleImage } from '@components/index';
 
 export const PlayerScreen = () => {
     const route = useRoute<PlayerScreenRouteProp>();
@@ -10,6 +11,12 @@ export const PlayerScreen = () => {
 
     return (
         <View style={styles.container}>
+            <CircleImage
+                url={player.headshot}
+                size={150}
+                resizeMode="cover"
+                imageRatio={0.9}
+            />
             <Text style={styles.text}>{player.name}</Text>
             <Text style={styles.text}>{player.age}</Text>
         </View>
@@ -19,6 +26,7 @@ export const PlayerScreen = () => {
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
+        marginVertical: 10,
     },
     text: {
         fontSize: 20,
