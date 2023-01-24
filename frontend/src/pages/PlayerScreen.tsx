2a0c894f9800c52fd3ded186a2c374ab340e2664
ben-rieth <benrieth3@gmail.com>
2023-01-24 16:00:53 -0600

    Update PlayerScreen

--- frontend/src/pages/PlayerScreen.tsx
+++ frontend/src/pages/PlayerScreen.tsx
@@ -2,14 +2,14 @@ import { PlayerScreenRouteProp } from '../types/Navigation';
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
 import { View, Text, StyleSheet } from 'react-native';
+import { Player } from './../types/Player';
 
 export const PlayerScreen = () => {
-    const route = useRoute<PlayerScreenRouteProp>(); //got the string a to send... still lots of errors
-    const player = route.params.player;
+    const route = useRoute<PlayerScreenRouteProp>();
+    const player: Player = route.params.player;
 
     return (
-        //could add undefined type to catch the error if no data was sent!e
-        <View>
+        <View style={styles.container}>
             <Text style={styles.text}>{player.name}</Text>
             <Text style={styles.text}>{player.age}</Text>
         </View>
@@ -19,24 +19,11 @@ export const PlayerScreen = () => {
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
-        flex: 1,
-    },
-    header: {
-        fontWeight: 'bold',
-        fontSize: 24,
-        textAlign: 'center',
     },
     text: {
         fontSize: 20,
         textAlign: 'center',
         fontWeight: 'bold',
         margin: 5,
-    },
-    circleImageLayout: {
-        width: 75,
-        height: 75,
-        borderRadius: 75 / 2,
-        borderColor: 'grey',
-        borderWidth: 2,
     },
 });
