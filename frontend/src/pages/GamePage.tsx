6a11ae38935224e0ebb5af2a428baa569e0c2b40
Kameron Eaton <kwe8364@utulsa.edu>
2023-01-24 13:22:19 -0600

    Create GamePage

---
+++ frontend/src/pages/GamePage.tsx
@@ -0,0 +1,21 @@
+// external imports
+import React, { useState } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { GameDisplay } from '../components/data/GameDisplay';
+//custom components
+import { Seperator, DaySchedule } from '../components/index';
+
+export const GamePage = () => {
+    return (
+        <View style = {styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 10,
+    },
+});
