6bc30c14ec64e956adb2029b70275c8ddfc4b7c9
Kameron Eaton <kwe8364@utulsa.edu>
2023-01-24 13:32:43 -0600

    Update

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,13 +1,18 @@
 // external imports
+import { addDays, startOfToday, format } from 'date-fns';
 import React, { useState } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, Text } from 'react-native';
 import { GameDisplay } from '../components/data/GameDisplay';
 //custom components
 import { Seperator, DaySchedule } from '../components/index';
+import { ORANGE } from '../styles/colors';
 
 export const GamePage = () => {
-    return (
+    const date = startOfToday();
+    const dateString = format(date, 'yyyy-MM-dd');
+        return (
         <View style = {styles.container}>
+            <Text style = {styles.textStyle}>{dateString}</Text>
 
         </View>
     )
@@ -18,4 +23,12 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: 10,
     },
+    textStyle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 30,
+        textDecorationLine: 'underline',
+        color: ORANGE,
+    },
+
 });
