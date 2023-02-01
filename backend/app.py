3fb54141e3b9eb05e131f51d5896380bf2580437
ben-rieth <benrieth3@gmail.com>
2023-02-01 16:52:00 -0600

    Switch local url to server url

--- backend/app.py
+++ backend/app.py
@@ -168,8 +168,8 @@ def get_team(id):
 
     return json.dumps(team)
 
-gpr = pickle.load(open('models\gpr_model_xs.pkl', 'rb'))
-league_stats = pd.read_csv('data\league_stats.csv')
+gpr = pickle.load(open('models/gpr_model_xs.pkl', 'rb'))
+league_stats = pd.read_csv('data/league_stats.csv')
 
 # Return predicted scores for a specified matchup
 @app.route('/score/<team1>/<team2>', methods=['GET'])
