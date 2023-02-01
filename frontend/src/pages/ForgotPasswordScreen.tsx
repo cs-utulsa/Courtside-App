ecd691aff59775607b04b8951c07fb904079fbd2
ben-rieth <benrieth3@gmail.com>
2023-02-01 16:01:54 -0600

    Add page on frontend to reset password

--- frontend/src/pages/ForgotPasswordScreen.tsx
+++ frontend/src/pages/ForgotPasswordScreen.tsx
@@ -1,10 +1,78 @@
+import {
+    AuthSubmitButton,
+    EmailInput,
+    ErrorBox,
+    LogoHeader,
+} from '@components/index';
+import { useAuth } from '@hooks/useAuth';
+import { Formik } from 'formik';
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import * as Yup from 'yup';
+
+const authSchema = Yup.object().shape({
+    email: Yup.string()
+        .email('Invalid Email Address')
+        .required('Email is required.'),
+});
 
 export const ForgotPasswordScreen = () => {
+    const { authError, forgotPassword } = useAuth();
+
     return (
-        <View>
-            <TextInput />
+        <View style={styles.container}>
+            <LogoHeader />
+            <Text style={styles.heading}>Reset Your Password</Text>
+            <Formik
+                initialValues={{ email: '' }}
+                validationSchema={authSchema}
+                onSubmit={async (values) => await forgotPassword(values.email)}
+            >
+                {({
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    values,
+                    errors,
+                    touched,
+                    isSubmitting,
+                    isValid,
+                }) => (
+                    <View style={styles.form}>
+                        <EmailInput
+                            changeFn={handleChange('email')}
+                            blurFn={handleBlur('email')}
+                            error={errors.email}
+                            touched={touched.email}
+                            value={values.email}
+                            disabled={isSubmitting}
+                        />
+                        <ErrorBox error={authError} />
+                        <AuthSubmitButton
+                            loading={isSubmitting}
+                            submitFn={handleSubmit}
+                            disabled={isSubmitting || !isValid}
+                            text="Send Password Reset Email"
+                        />
+                    </View>
+                )}
+            </Formik>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    form: {
+        width: '100%',
+        alignItems: 'center',
+    },
+    heading: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+});
