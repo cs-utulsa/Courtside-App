c937c005172447c964b2f6c8cb201f1404597b0d
ben-rieth <benrieth3@gmail.com>
2023-02-15 17:11:12 -0600

    Change view into scroll view for player screen

--- frontend/src/pages/PlayerScreen.tsx
+++ frontend/src/pages/PlayerScreen.tsx
@@ -1,26 +1,19 @@
 import { PlayerScreenRouteProp } from '../types/Navigation';
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Player } from './../types/Player';
 import { Team } from './../types/Team';
 import { CircleImage } from '@components/index';
 import { useNavigation } from '@react-navigation/native';
 import { RosterNavigationProp } from './../types/Navigation';
 
-import {
-    StatLeaderboard,
-    PrimaryButton,
-    FullError,
-    Seperator,
-} from '@components/index';
-
+import { PrimaryButton } from '@components/index';
 
 /**
  * This screen shows the data for one player.
  * The player data is passed through a navigation parameter
  */
-
 
 export const PlayerScreen = () => {
     const { push } = useNavigation<RosterNavigationProp>();
@@ -28,54 +21,46 @@ export const PlayerScreen = () => {
     const player: Player = route.params.player;
     const teamback: Team = route.params.team;
     function navigateToSelectionScreen() {
-       // const navigation = useNavigation(); 
-      push('Team',{team: teamback});
-
+        // const navigation = useNavigation();
+        push('Team', { team: teamback });
     }
-    
+
     return (
-        
-        <View style={styles.container}>
-            <PrimaryButton
-                onPress={navigateToSelectionScreen}
-                text="Back"
-            />
+        <ScrollView contentContainerStyle={styles.container}>
+            <PrimaryButton onPress={navigateToSelectionScreen} text="Back" />
             <CircleImage
                 url={player.headshot}
                 size={150}
                 resizeMode="cover"
                 imageRatio={0.9}
             />
-             <Text style={styles.text}>{player.name}</Text>
-             <Text style={styles.text}>{player.team}</Text>
-            
-            <View style={styles.leaderboardBlock}>
-           <View> 
-            <Text style={styles.text}>Position:</Text>
-             <Text style={styles.listtext}>{player.position}</Text>
-             <Text style={styles.text}>Height:</Text>
-            <Text style={styles.listtext}> {player.height}</Text>
-            <Text style={styles.text}>Weight:</Text>
-            <Text style={styles.listtext}> {player.weight}</Text>
-            <Text style={styles.text}>Age:</Text>
-            <Text style={styles.listtext}> {player.age}</Text>
-            </View>
+            <Text style={styles.text}>{player.name}</Text>
+            <Text style={styles.text}>{player.team}</Text>
 
-            <View style={styles.statstwo}>
-            <Text style={styles.text}>Jersey No. : </Text>
-            <Text style={styles.listtext}> {player.number}</Text>
-            <Text style={styles.text}>Career: </Text>
-            <Text style={styles.listtext}> {player.experience}</Text>
-            <Text style={styles.text}>Draft Pick:</Text>
-            <Text style={styles.listtext}> {player.draft}</Text>
-            <Text style={styles.text}>Country:</Text>
-            <Text style={styles.listtext}> {player.country}</Text>
-            
+            <View style={styles.leaderboardBlock}>
+                <View>
+                    <Text style={styles.text}>Position:</Text>
+                    <Text style={styles.listtext}>{player.position}</Text>
+                    <Text style={styles.text}>Height:</Text>
+                    <Text style={styles.listtext}> {player.height}</Text>
+                    <Text style={styles.text}>Weight:</Text>
+                    <Text style={styles.listtext}> {player.weight}</Text>
+                    <Text style={styles.text}>Age:</Text>
+                    <Text style={styles.listtext}> {player.age}</Text>
+                </View>
+
+                <View style={styles.statstwo}>
+                    <Text style={styles.text}>Jersey No. : </Text>
+                    <Text style={styles.listtext}> {player.number}</Text>
+                    <Text style={styles.text}>Career: </Text>
+                    <Text style={styles.listtext}> {player.experience}</Text>
+                    <Text style={styles.text}>Draft Pick:</Text>
+                    <Text style={styles.listtext}> {player.draft}</Text>
+                    <Text style={styles.text}>Country:</Text>
+                    <Text style={styles.listtext}> {player.country}</Text>
+                </View>
             </View>
-           </View>
-            
-            
-        </View>
+        </ScrollView>
     );
 };
 
@@ -86,25 +71,25 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 20,
-       // textAlign: '',
+        // textAlign: '',
         fontWeight: 'bold',
         margin: 5,
-    }, 
+    },
     statstwo: {
         fontSize: 20,
         textAlign: 'right',
         fontWeight: 'bold',
         marginLeft: 40,
-    }, 
+    },
     listtext: {
         fontSize: 20,
-       // textAlign: '',
-      //  fontWeight: 'bold',
+        // textAlign: '',
+        //  fontWeight: 'bold',
         margin: 5,
-    },  
-      leaderboardBlock: {
+    },
+    leaderboardBlock: {
         flexDirection: 'row',
-       // alignItems: 'center',
+        // alignItems: 'center',
         backgroundColor: '#DEDEDE',
         paddingVertical: 8,
         paddingHorizontal: 20,
