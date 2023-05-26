2ac84b6529b42df82dd2cb0f6b2d2335d079698d
Ben Riethmeier <benrieth3@gmail.com>
2023-05-25 19:01:01 -0500

    Fix score prediction view

--- frontend/src/components/data/GameScorePrediction.tsx
+++ frontend/src/components/data/GameScorePrediction.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from 'react';
-import { BarChart, ProgressChart } from 'react-native-chart-kit';
 import { Card } from '../misc/Card';
 import { useScorePrediction } from '@hooks/queries/useScorePrediction';
 import { ErrorBox } from '../error/ErrorBox';
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { ThemeText } from '@components/misc/ThemeText';
-import hexToRgba from 'hex-to-rgba';
 import { ScorePrediction } from './../../types/Game';
 import { NBA_ICONS } from '@constants/icons';
 import { CircleImage } from '@components/images/CircleImage';
@@ -40,65 +38,43 @@ export const GameScorePrediction: FC<GameScorePredictionProps> = ({
             </ThemeText>
             {isSuccess && (
                 <>
-                    <BarChart
-                        data={{
-                            labels: Object.keys(data),
-                            datasets: [
-                                {
-                                    data: Object.values(data).map((val) =>
-                                        Number(val.score.toFixed(0))
-                                    ),
-                                },
-                            ],
-                        }}
-                        showBarTops
-                        showValuesOnTopOfBars
-                        width={250}
-                        height={200}
-                        chartConfig={{
-                            color: () => colors.primary,
-                            backgroundGradientFrom: colors.card,
-                            backgroundGradientTo: colors.card,
-                            barPercentage: 1,
-                        }}
-                        yAxisSuffix=""
-                        yAxisLabel=""
-                        withVerticalLabels
+                    <CircleImage
+                        url={
+                            NBA_ICONS.find(
+                                (icon) => icon.code === getWinner(data)
+                            )?.logo ?? ''
+                        }
+                        size={120}
+                        borderColor={colors.primary}
                     />
-                    <ThemeText>
-                        {getWinner(data)} has a&nbsp;
-                        {(data[getWinner(data)].win_pct * 100).toFixed(0)}%
-                        chance to win!
-                    </ThemeText>
-                    <View style={styles.winPercentContainer}>
-                        <ProgressChart
-                            data={{
-                                labels: [getWinner(data)],
-                                data: [data[getWinner(data)].win_pct],
-                            }}
-                            width={250}
-                            height={170}
-                            radius={62}
-                            chartConfig={{
-                                color: (opacity = 1) =>
-                                    hexToRgba(colors.primary, opacity),
-                                backgroundGradientFrom: colors.card,
-                                backgroundGradientTo: colors.card,
-                                backgroundColor: 'blue',
-                            }}
-                            hideLegend
-                        />
-                        <View style={styles.imageContainer}>
-                            <CircleImage
-                                url={
-                                    NBA_ICONS.find(
-                                        (icon) => icon.code === getWinner(data)
-                                    )?.logo ?? ''
-                                }
-                                size={120}
-                            />
+                    <View style={styles.scoreBox}>
+                        <View style={styles.teamBox}>
+                            <ThemeText style={styles.scoreText}>
+                                {Math.floor(data[teams[0]].score)}
+                            </ThemeText>
+                            <ThemeText style={styles.teamText}>
+                                {teams[0]}
+                            </ThemeText>
+                        </View>
+                        <ThemeText style={styles.scoreText}>-</ThemeText>
+                        <View style={styles.teamBox}>
+                            <ThemeText style={styles.scoreText}>
+                                {Math.floor(data[teams[1]].score)}
+                            </ThemeText>
+                            <ThemeText style={styles.teamText}>
+                                {teams[1]}
+                            </ThemeText>
                         </View>
                     </View>
+                    <ThemeText
+                        style={[styles.teamText, { marginHorizontal: 25 }]}
+                    >
+                        <ThemeText>{getWinner(data)} has a&nbsp;</ThemeText>
+                        <ThemeText style={[styles.scoreText]} primary>
+                            {(data[getWinner(data)].win_pct * 100).toFixed(0)}%
+                        </ThemeText>
+                        <ThemeText>&nbsp;chance to win!</ThemeText>
+                    </ThemeText>
                 </>
             )}
             {isError && <ErrorBox error="Cannot receive score prediction" />}
@@ -123,5 +99,22 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 28,
         left: 65,
+    },
+    scoreBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '50%',
+    },
+    scoreText: {
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    teamText: {
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    teamBox: {
+        alignItems: 'center',
     },
 });
